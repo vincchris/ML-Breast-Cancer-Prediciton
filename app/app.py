0cd66e512efc35d00789218c4defdfe21974b47b
irisvyriel <vchristianthen@gmail.com.com>
2024-11-05 11:25:34 +0700

    fix error pkl

--- app/app.py
+++ app/app.py
@@ -6,9 +6,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
 # Load models and scaler
-logistic_model = pickle.load(open("/model/logistic_model.pkl", "rb"))
-random_forest_model = pickle.load(open("/model/random_forest_model.pkl", "rb"))
-scaler = pickle.load(open("/model/scaler.pkl", "rb"))
+logistic_model = pickle.load(open("model/logistic_model.pkl", "rb"))
+random_forest_model = pickle.load(open("model/random_forest_model.pkl", "rb"))
+scaler = pickle.load(open("model/scaler.pkl", "rb"))
 
 # Sidebar for model selection
 st.sidebar.title("Model Selection")
